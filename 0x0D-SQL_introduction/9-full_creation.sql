cc740675fe8a5c42affb6927be1751e91f69cd3e
Binadam442 <sanusiadam0000@gmail.com>
2024-02-14 08:22:16 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,7 +10,7 @@
 --      id = 1, name = “Adam”, score = 10
 --      id = 2, name = “Isa”, score = 3
 --      id = 3, name = “Baba”, score = 14
---      id = 4, name = "Sunusi”, score = 8
+--      id = 4, name = "Sunu”, score = 8
 
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
@@ -19,4 +19,4 @@ INSERT INTO second_table (id, name, score)
 VALUES (1, "Adam", 10),
 (2, "Isa", 3),
 (3, "Baba", 14),
-(4, "Sunusi", 8);
+(4, "Sunu", 8);
